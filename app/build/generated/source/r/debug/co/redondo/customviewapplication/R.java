311941471e3833067aa3e1b3e19106fb1eb8e111
RaquelRedondo <raquelro22@gmail.com>
2016-05-30 03:42:00 +0100

    Adding button for random colors

--- app/build/generated/source/r/debug/co/redondo/customviewapplication/R.java
+++ app/build/generated/source/r/debug/co/redondo/customviewapplication/R.java
@@ -1806,6 +1806,7 @@ containing a value of this type.
         public static final int abc_shareactionprovider_share_with_application=0x7f060011;
         public static final int abc_toolbar_collapse_description=0x7f060012;
         public static final int app_name=0x7f060014;
+        public static final int btn_label=0x7f060015;
         public static final int status_bar_notification_info_overflow=0x7f060013;
     }
     public static final class style {
--- app/src/main/java/co/redondo/customviewapplication/MainActivity.java
+++ app/src/main/java/co/redondo/customviewapplication/MainActivity.java
@@ -1,13 +1,29 @@
 package co.redondo.customviewapplication;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CustomView customView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        customView = (CustomView) findViewById(R.id.custView);
+    }
+
+    public void btnPressed(View view){
+        //Update the view
+        Random rnd = new Random();
+        int color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+        customView.setCircleColor(color);
+        color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+        customView.setLabelColor(color);
     }
 }
